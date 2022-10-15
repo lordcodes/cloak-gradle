1a83568d9a9dfc59d1ba0abf93c9b1b0ec7ee085
Andrew Lord <andrew@lordcodes.com>
2022-10-16 00:24:33 +0100

    settings.gradle.kts files

---
+++ cloak-gradle-plugin/settings.gradle.kts
@@ -0,0 +1,30 @@
+@file:Suppress("UnstableApiUsage")
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        mavenCentral()
+        google()
+    }
+
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
+
+rootProject.name = "cloak-gradle-plugin"
+
+include(":plugin")
---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+@file:Suppress("UnstableApiUsage")
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
+
+rootProject.name = "cloak-gradle"
+
+includeBuild("cloak-plugin")
+
+include(":example")
